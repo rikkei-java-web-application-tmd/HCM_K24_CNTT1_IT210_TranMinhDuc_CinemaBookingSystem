8d0ee30f44e119b98a2a2ccfbe823a8bf6361c52
breakBlogHubVercel <breakgamer2601@gmail.com>
2026-05-18 08:30:08 +0700

    init Project

---
+++ src/test/java/com/re/cinemabooking/CinemaBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.re.cinemabooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
